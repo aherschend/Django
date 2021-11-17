0db25cb4ab3519e239d39742ba2eee5c9072ccfd
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-11-17 17:17:42 -0600

    created html

--- MainApp/models.py
+++ MainApp/models.py
@@ -13,4 +13,12 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
+    #change Entrys to Entries
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..." # the ... indicates that there is more to follow
+
     
